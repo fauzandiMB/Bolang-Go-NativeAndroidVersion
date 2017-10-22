9e8f6ef92e7216af7806311b5aea6f8f79a68fe2
fauzandimb <fauzandib@gmail.com>
2017-10-22 21:36:52 +0700

    Create Langkah 1 UI

--- app/src/main/java/go/bolang/www/bolang_go/BuatPermainanActivity.java
+++ app/src/main/java/go/bolang/www/bolang_go/BuatPermainanActivity.java
@@ -1,7 +1,9 @@
 package go.bolang.www.bolang_go;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BuatPermainanActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class BuatPermainanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buat_permainan);
+    }
+
+    public void toGabungPermainan2(View view) {
+        Intent intent = new Intent(BuatPermainanActivity.this, BuatPermainan2Activity.class);
+        startActivity(intent);
     }
 }
