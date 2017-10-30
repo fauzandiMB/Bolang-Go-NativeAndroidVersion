aa86b5049c66f33942a8c12af23a6355e0cabac7
ArifBudiman19 <arif.budiman41@ui.ac.id>
2017-10-30 11:59:43 +0700

    add login features

---
+++ app/src/main/java/go/bolang/www/bolang_go/Constant.java
@@ -0,0 +1,10 @@
+package go.bolang.www.bolang_go;
+
+/**
+ * Created by arifb on 30-Oct-17.
+ */
+
+public class Constant {
+    public static final int RC_SIGN_IN = 12;
+    public static final String TAG = "NGEBOLANGAJA";
+}
--- app/src/main/java/go/bolang/www/bolang_go/MainActivity.java
+++ app/src/main/java/go/bolang/www/bolang_go/MainActivity.java
@@ -2,16 +2,33 @@ package go.bolang.www.bolang_go;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+
+
 public class MainActivity extends AppCompatActivity {
+    private Button logoutBtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null){
+                    finish();
+                }
+            }
+        };
 
         Button button = (Button) findViewById(R.id.btn_gabung_permainan);
         button.setOnClickListener(new View.OnClickListener() {
@@ -21,6 +38,17 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button logoutBtn = (Button) findViewById(R.id.btn_logout);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                if(mAuth.getCurrentUser() == null){
+                    finish();
+                }
+            }
+        });
     }
 
    public void toBuatPermainan(View view) {
@@ -28,8 +56,5 @@ public class MainActivity extends AppCompatActivity {
        startActivity(intent);
    }
 
-    public void toGabungPermainan(View view) {
-        //Intent intent = new Intent(MainActivity.this, GabungPermainanActivity.class);
-        //startActivity(intent);
-    }
+
 }
