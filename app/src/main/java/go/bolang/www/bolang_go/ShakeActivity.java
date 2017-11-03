a2c3cdc24f1697ae884043be938699daad565653
DESKTOP-T97DDVR\Alvin <alvin>
2017-11-03 11:00:27 +0700

    initial shake

---
+++ app/src/main/java/go/bolang/www/bolang_go/ShakeActivity.java
@@ -0,0 +1,13 @@
+package go.bolang.www.bolang_go;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shake);
+    }
+}
