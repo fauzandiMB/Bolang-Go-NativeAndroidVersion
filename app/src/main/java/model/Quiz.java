c4267291724057e5e9ad0672d5d9913a1a5b2c4a
ArifBudiman19 <arif.budiman41@ui.ac.id>
2017-11-18 13:02:04 +0700

    add quiz model

---
+++ app/src/main/java/model/Quiz.java
@@ -0,0 +1,60 @@
+package model;
+
+import android.util.Log;
+
+import java.io.Serializable;
+
+/**
+ * Created by arifb on 18-Nov-17.
+ */
+
+public class Quiz implements Serializable {
+
+    public String question;
+    public String[] choices = new String[4];
+    public Double rightIndex;
+
+    public Quiz(String question, String[] choices, Double rightIndex) {
+        this.question = question;
+        this.choices = choices;
+        this.rightIndex = rightIndex;
+    }
+
+    public Quiz(String question) {
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String[] getChoices() {
+        return choices;
+    }
+
+    public void setChoice(String choice, int index){
+        try {
+            this.choices[index] = choice;
+        }catch (Exception e){
+            Log.e(this.getClass().getName(), "error idk cari sendiri ya");
+        }
+    }
+
+    public void setChoices(String[] choices) {
+        this.choices = choices;
+    }
+
+    public Double getRightIndex() {
+        return rightIndex;
+    }
+
+    public void setRightIndex(Double rightIndex) {
+        this.rightIndex = rightIndex;
+    }
+
+
+}
