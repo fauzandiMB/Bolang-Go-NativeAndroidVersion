e4e0ed87afd2c4745af8ab13e6393c3a0a03d7bc
ArifBudiman19 <arif.budiman41@ui.ac.id>
2017-11-18 13:14:30 +0700

    add tresure model

---
+++ app/src/main/java/model/Treasure.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.io.Serializable;
+
+/**
+ * Created by arifb on 18-Nov-17.
+ */
+
+public class Treasure implements Serializable {
+
+    public Double maxCoin;
+    public Double timeCount = 60.0;
+
+    public Treasure(Double maxCoin) {
+        this.maxCoin = maxCoin;
+    }
+
+    public Double getMaxCoin() {
+        return maxCoin;
+    }
+
+    public void setMaxCoin(Double maxCoin) {
+        this.maxCoin = maxCoin;
+    }
+
+    public Double getTimeCount() {
+        return timeCount;
+    }
+
+    public void setTimeCount(Double timeCount) {
+        this.timeCount = timeCount;
+    }
+
+}
