dedb2ecb3bea8b481be2766a6bb308b7784fa6df
DESKTOP-T97DDVR\Alvin <alvin>
2017-12-13 13:09:47 +0700

    tambah result activity

---
+++ app/src/main/java/go/bolang/www/bolang_go/ResultActivity.java
@@ -0,0 +1,16 @@
+package go.bolang.www.bolang_go;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+}
